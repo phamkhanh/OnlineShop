2aa6b0b2dd439c175bb5f991b7aa1f382e3356da
Khanh Pham <phamkhanh1811@gamil.com>
2018-01-03 22:56:23 +0700

    todo unit test. and remove unit test not show test case on test window. be later

---
+++ App.Service/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Service
+{
+    //[TestClass]
+    //class PostCategoryRepositoryTest
+    //{
+    //    IDbFactory _dbFactory;
+    //    IPostCategoryRepository _postCategoryRepository;
+    //    IUnitOfWork _unitOfWork;
+
+    //    [TestInitialize]
+    //    public void Initialize()
+    //    {
+    //        _dbFactory = new DbFactory();
+    //        _postCategoryRepository = new PostCategoryRepository(_dbFactory);
+    //        _unitOfWork = new UnitOfWork(_dbFactory);
+    //    }
+
+    //    [TestMethod]
+    //    public void PostCategory_Repository_Create()
+    //    {
+    //        PostCategory postCategory = new PostCategory();
+    //        postCategory.Name = "Test category";
+    //        postCategory.Alias = "Test-category";
+    //        postCategory.Status = true;
+    //        var result = _postCategoryRepository.Add(postCategory);
+    //        _unitOfWork.Commit();
+
+    //        Assert.IsNotNull(result);
+    //        Assert.AreEqual(1, result.ID);
+    //    }
+    //}
+}
